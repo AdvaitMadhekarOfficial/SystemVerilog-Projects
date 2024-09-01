0a1c25d0ef677bbe10517c60a89a4c5e681cdbed
Advait Madhekar <adimadhekar88@gmail.com>
2024-09-01 14:59:09 -0700

    add fifo

---
+++ personal_projects/fifo_32x16/sync_sent_to_charles/design.sv
@@ -0,0 +1,89 @@
+`timescale 1ms/1ms
+
+interface BUS_IF(
+  input bit CLK
+);
+  bit WR;
+  bit RD;
+  bit [15:0] DOUT;
+  bit [15:0] DIN;
+  bit QUEUE_EMPTY;
+  bit QUEUE_FULL;
+endinterface:BUS_IF
+
+
+module FIFO_32x16 #(parameter N = 32)(BUS_IF IO_DATA);
+
+  bit [15:0] FIFO_STACK[N]; //Datastorage
+  bit [4:0] RD_PTR = 5'b00000;
+  bit [4:0] WR_PTR = 5'b00000;
+  bit QUEUE_FIRST_ITER = 1'b0;
+  bit [4:0] QUEUE_OCCUPANCY = 5'b0;
+  
+  wire W_QUEUE_EMPTY; 
+  wire W_QUEUE_FULL;
+  
+  /*initial
+    begin
+      RD_PTR = 0;
+      WR_PTR = 0;
+    end
+   */
+
+  assign W_QUEUE_EMPTY = (QUEUE_OCCUPANCY == 0);
+  assign W_QUEUE_FULL = (QUEUE_OCCUPANCY == (N - 1));
+  
+  assign IO_DATA.QUEUE_EMPTY = W_QUEUE_EMPTY;
+  assign IO_DATA.QUEUE_FULL = W_QUEUE_FULL;
+
+  //UPDATE VARS
+  always @(posedge IO_DATA.CLK)
+    begin:UPDATE
+      //QUEUE_EMPTY = (WR_PTR == RD_PTR);
+      //QUEUE_EMPTY = (QUEUE_OCCUPANCY == 0);
+      
+      //QUEUE_FULL = (WR_PTR == RD_PTR - 1) & ~QUEUE_EMPTY;
+      //QUEUE_FULL = (QUEUE_OCCUPANCY == N);
+      //IO_DATA.QUEUE_FULL = QUEUE_FULL;
+      
+      if(WR_PTR == N)
+        WR_PTR = 5'b0;
+      if(RD_PTR == N)
+        RD_PTR = 5'b0;
+    end:UPDATE
+  
+  //READ OPERATION
+  always @(posedge IO_DATA.CLK)
+    begin
+      if(IO_DATA.RD)
+        begin
+          //$write("\nRDPTR: %d\nQE: %b\nFS[RP]: %4h\n==x==\n", RD_PTR, QUEUE_EMPTY, FIFO_STACK[RD_PTR]);
+          if(W_QUEUE_EMPTY == 0)
+            begin
+              //$write("\nENTRY\n==x==\n");
+              IO_DATA.DOUT = FIFO_STACK[RD_PTR];
+              RD_PTR = RD_PTR + 1;
+              QUEUE_OCCUPANCY -= 1;
+            end
+        end
+    end
+
+  always @(posedge IO_DATA.CLK) begin
+    $display (" RD = %0b WR = %0b WR_PTR = %0d RD_PTR = %0d FULL = %0b EMPTY = %0b OCCPUANCY = %0d \n", IO_DATA.RD, IO_DATA.WR, WR_PTR , RD_PTR, W_QUEUE_FULL, W_QUEUE_EMPTY, QUEUE_OCCUPANCY );
+  end  
+  
+  //WRITE OPERATION
+  always @(posedge IO_DATA.CLK)
+    if(IO_DATA.WR)
+      begin
+        if(W_QUEUE_FULL == 0)
+          begin
+            FIFO_STACK[WR_PTR] = IO_DATA.DIN;
+            WR_PTR = WR_PTR + 1;
+            QUEUE_OCCUPANCY += 1;
+          end
+      end
+
+
+
+endmodule
---
+++ personal_projects/fifo_32x16/synchronous_read_write/testbench.sv
@@ -0,0 +1,186 @@
+package TB_PKG;
+
+  typedef struct {
+    realtime RTIME; 
+    int TAG;
+    bit READ_OR_WRITE;
+    bit [15:0] DATA;
+    bit QUEUE_EMPTY;
+    bit QUEUE_FULL;
+  } SCOREBOARD_OBJ;
+
+  parameter int Nburst = 500;
+
+  SCOREBOARD_OBJ scorecard[Nburst];
+  int curr_ptr = 0;
+
+  typedef virtual BUS_IF VRWIF_t;
+  
+  import uvm_pkg::*;
+
+  class DRIVER extends uvm_driver;
+  
+    VRWIF_t VRWIF;
+    
+    rand bit READ_OR_WRITE;
+    
+    rand bit [15:0] DATA;
+    
+    //Makes sure Writes are 70% likely compared to reads
+    constraint R_O_W_DECIDE {
+      READ_OR_WRITE dist{
+        1'b1 := 7,
+        1'b0 := 3
+      };
+    }
+    
+    function new(string INAME, VRWIF_t RWIF_arg);
+      super.new(INAME, null);
+      VRWIF = RWIF_arg;
+    endfunction:new
+    
+    
+    task WRITE_DATA(int TAG);
+      VRWIF.WR = 1'b1;
+      VRWIF.RD = 1'b0;
+      VRWIF.DIN = DATA;
+      if(VRWIF.QUEUE_FULL)
+        begin
+          scorecard[curr_ptr] = '{$time, TAG, READ_OR_WRITE, 16'b0, 0, 1};
+          curr_ptr += 1;
+        end
+      else
+        begin
+          scorecard[curr_ptr] = '{$time, TAG, READ_OR_WRITE, DATA, 0, 0};
+          curr_ptr += 1;
+        end
+    endtask:WRITE_DATA;
+    
+    task READ_DATA(int TAG);
+      VRWIF.RD = 1'b1;
+      VRWIF.WR = 1'b0;
+      if(VRWIF.QUEUE_EMPTY)
+        begin
+          if(scorecard[curr_ptr - 1].DATA != VRWIF.DOUT && scorecard[curr_ptr - 1].QUEUE_EMPTY != 1)
+            begin 
+              scorecard[curr_ptr] = '{$time, TAG, READ_OR_WRITE, VRWIF.DOUT, 0, 0};
+              curr_ptr += 1;
+            end
+          else
+            begin
+              scorecard[curr_ptr] = '{$time, TAG, READ_OR_WRITE, 16'b0, 1, 0};
+              curr_ptr += 1;
+            end
+        end
+      else
+        begin
+          scorecard[curr_ptr] = '{$time, TAG, READ_OR_WRITE, VRWIF.DOUT, 0, 0};
+          curr_ptr += 1;
+        end
+    endtask:READ_DATA;
+    
+    task ENV_OF_TASKS;
+      for(int i = 0; i < Nburst; i++)
+        begin
+          this.randomize();
+          //READ_OR_WRITE = 1'b1;
+          if(READ_OR_WRITE == 0) // READ
+            begin:CALL_READ
+              #20ms
+              this.READ_DATA(i);
+            end:CALL_READ
+          else //WRITE
+            begin:CALL_WRITE
+              #20ms
+              this.WRITE_DATA(i);
+            end:CALL_WRITE
+        end
+    endtask:ENV_OF_TASKS;
+  
+  endclass: DRIVER
+
+  class MONITOR extends uvm_monitor;
+    
+    VRWIF_t VRWIF;
+    
+    function new(string INAME, VRWIF_t RWIF_arg);
+      super.new(INAME, null);
+      VRWIF = RWIF_arg;
+    endfunction:new
+  	
+    task DISPLAY_RESULTS;
+      $display("@ TIME TAG READ_OR_WRITE DATA QUEUE_EMPTY QUEUE_FULL");
+      for(int i = 0; i < Nburst; i++)
+        begin
+          if(scorecard[i].QUEUE_EMPTY == 1)
+            begin
+              $display("ERROR: FIFO EMPTY");
+            end
+          else if(scorecard[i].QUEUE_FULL == 1)
+            begin
+              $display("ERROR: FIFO FULL");
+            end
+          else
+            begin
+              $display("@ %d, %d :: %b :: %4h", scorecard[i].RTIME, scorecard[i].TAG, scorecard[i].READ_OR_WRITE, scorecard[i].DATA);
+            end
+        end
+      //DUMP SCOREBOARD
+      $display("\n\nSCORECARD: \n\n%p", scorecard);
+    endtask:DISPLAY_RESULTS;
+  	
+  endclass: MONITOR
+
+
+  class ENVIRONMENT extends uvm_env;
+  
+    DRIVER DRV;
+    MONITOR MON;
+    
+    VRWIF_t VRWIF;
+    
+    function new(string INAME, VRWIF_t RWIF_arg);
+      super.new(INAME, null);
+      VRWIF = RWIF_arg;
+      DRV = new("DRIVER", RWIF_arg);
+      MON = new("MONITOR", RWIF_arg);
+    endfunction:new
+    
+  endclass: ENVIRONMENT
+
+endpackage: TB_PKG;
+
+
+module FIFO_32x16_TB;
+  
+  import TB_PKG::*;
+  
+  bit CLK = 1'b0;
+  
+  BUS_IF RWIF(CLK);
+  
+  FIFO_32x16 DUT(.IO_DATA(RWIF));
+  
+  ENVIRONMENT ENV;
+  
+  initial
+    begin:TEST_SUITE
+      ENV = new("ENVIRONMENT", RWIF);
+      
+      ENV.DRV.randomize();
+      
+      ENV.DRV.ENV_OF_TASKS();
+      ENV.MON.DISPLAY_RESULTS();
+      $display("\n\nFIFO_STACK: \n\n%p\nWR_PTR: %5b\nRD_PTR: %5b\n\n\n", DUT.FIFO_STACK, DUT.WR_PTR, DUT.RD_PTR);
+    end:TEST_SUITE
+  
+  //CLK GEN
+  initial
+    repeat(Nburst)
+      begin:CLOCK
+        #10 CLK = 1'b1;
+        #10 CLK = 1'b0;
+      end:CLOCK  
+  
+endmodule
+
